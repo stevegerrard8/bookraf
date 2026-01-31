6ed63297f4ca59256e0bf2c01cae0ec46edb5a50
Serhat UYAR <109991190+stevegerrard8@users.noreply.github.com>
2026-01-31 11:26:12 +0300

    Create data.js

---
+++ data.js
@@ -0,0 +1,34 @@
+const parties = [
+  {
+    name: "Cumhuriyet Halk Partisi",
+    founded: 1923,
+    ideology: "Sosyal demokrasi, Kemalizm",
+    status: "Aktif"
+  },
+  {
+    name: "Adalet ve Kalkınma Partisi",
+    founded: 2001,
+    ideology: "Muhafazakâr demokrasi",
+    status: "Aktif"
+  },
+  {
+    name: "Milliyetçi Hareket Partisi",
+    founded: 1969,
+    ideology: "Türk milliyetçiliği",
+    status: "Aktif"
+  }
+];
+
+const container = document.getElementById("parties");
+
+parties.forEach(party => {
+  const div = document.createElement("div");
+  div.className = "party";
+  div.innerHTML = `
+    <strong>${party.name}</strong><br>
+    <small>Kuruluş: ${party.founded}</small><br>
+    <small>İdeoloji: ${party.ideology}</small><br>
+    <small>Durum: ${party.status}</small>
+  `;
+  container.appendChild(div);
+});
